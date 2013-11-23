d0e1a8be1e6057569c47293b0e949b14e3a7a591
Robb Shecter <robb@weblaws.org>
2013-11-23 15:57:09 -0800

    Switched to the Fixed Navbar layout.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :setup_info
+
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, :alert => exception.message
+  end
+
+
+  def setup_info
+    @title = 'Bot Proof 3D — Model Checking and Rating'
   end
 
 end
