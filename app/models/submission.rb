ddbf713681a648da8032bbe52bcaf901941be472
Jon Austin <jon.i.austin@gmail.com>
2013-11-24 12:05:00 -0800

    upload to these services checkboxes

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,4 +1,11 @@
 class Submission < ActiveRecord::Base
+  attr_accessor :apis
+
+  def initialize
+    self.apis = %W(Thingiverse Ponoko Shapeways Github)
+    super
+  end
+
   belongs_to :user
 
   mount_uploader :image, ImageUploader
